7fdce40485284993631672ed7f0559119a1d43af
Kael Johnson <kaeljohnson6@gmail.com>
2024-07-11 17:51:48 -0500

    Cleaned up comments

--- GameV1/source/Camera/PlayerCamera.cpp
+++ GameV1/source/Camera/PlayerCamera.cpp
@@ -8,47 +8,6 @@ m_topTargetScreenBound(0.25f), m_bottomTargetScreenBound(0.75f), m_lookAheadFact
 {
 }
 
-//void PlayerCamera::Update(const double interpolation)
-//{
-//	double targetX = Engine::Lerp(ptrCameraTarget->GetPhysicsBody()->GetCenterPrevX(), ptrCameraTarget->GetPhysicsBody()->GetCenterXInMeters(), interpolation);
-//	double targetY = Engine::Lerp(ptrCameraTarget->GetPhysicsBody()->GetCenterPrevY(), ptrCameraTarget->GetPhysicsBody()->GetCenterYInMeters(), interpolation);
-//
-//	// bool centerPlayer = (targetX <= m_offsetX + (m_widthInMeters / 2.0f) && ptrCameraTarget->refKeyStates.at(Engine::D_KEY_DOWN)) || 
-//	// 	(targetX >= m_offsetX + (m_widthInMeters / 2.0f) && (ptrCameraTarget->refKeyStates.at(Engine::A_KEY_DOWN)));
-//
-//	double desiredLookAhead = ptrCameraTarget->DirectionFacing * m_lookAheadFactor;
-//
-//	m_lookAhead = Engine::Lerp(m_lookAhead, desiredLookAhead, interpolation * 0.001);
-//
-//	targetX += m_lookAhead;
-//
-//	// Desired camera position based on the target's position
-//	double desiredCameraTopLeftX = (float)targetX - (m_widthInMeters / 2.0);
-//	double desiredCameraTopLeftY = (float)targetY - (m_heightInMeters / 2.0);
-//
-//	m_offsetX += (desiredCameraTopLeftX - m_offsetX); // * (m_smoothingFactor / SCALEX);
-//	// m_offsetY += (desiredCameraTopLeftY - m_offsetY) * (0.001f / SCALEY);
-//	
-//	if (targetY > m_offsetY + ((m_heightInMeters) * m_bottomTargetScreenBound))
-//	{
-//		m_offsetY = (float)targetY - ((m_heightInMeters) * m_bottomTargetScreenBound);
-//	}
-//	else if (targetY < m_offsetY + ((m_heightInMeters) * m_topTargetScreenBound))
-//	{
-//		m_offsetY = (float)targetY - ((m_heightInMeters) * m_topTargetScreenBound);
-//	}
-//	else if (m_smoothingOn)
-//	{
-//		m_offsetY += (desiredCameraTopLeftY - m_offsetY) * (m_smoothingFactor / SCALEY);
-//	}
-//	else
-//	{
-//		m_offsetY = desiredCameraTopLeftY;
-//	}
-//
-//	if (m_clampingOn) Clamp();
-//}
-
 void PlayerCamera::Update(const double interpolation)
 {
     double targetX = Engine::Lerp(ptrCameraTarget->GetPhysicsBody()->GetCenterPrevX(), ptrCameraTarget->GetPhysicsBody()->GetCenterXInMeters(), interpolation);
