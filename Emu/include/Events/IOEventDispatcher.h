f9dba21c4ca4eb4ca5391fa5db45f64d64c47072
Kael Johnson <kaeljohnson6@gmail.com>
2026-01-03 15:46:00 -0600

    doxygen comments

--- Emu/include/Events/IOEventDispatcher.h
+++ Emu/include/Events/IOEventDispatcher.h
@@ -11,11 +11,26 @@ namespace Engine
 {
     using IOEventQueue = std::queue<IOEvent>;
 
+    /**
+	* @brief Dispatches SDL2 events to the IOEventQueue.
+    * 
+	* This class listens for SDL2 events and translates them into custom IOEvent types,
+    */
 	class IOEventDispatcher
 	{
     public:
         IOEventDispatcher();
+
+        /**
+		* @brief Polls for SDL2 events and dispatches them to the event queue.
+        * 
+		* This function should be called every frame.
+        */
         void PollEvents();
+
+        /**
+		* @brief Sets up the eventQ.
+        */
 		void SetEventQueue(IOEventQueue* eventQ);
     private:
         void dispatchQuitEvent();   
@@ -27,10 +42,10 @@ namespace Engine
         void dispatchMouseButtonUpEvent(SDL_MouseButtonEvent& mouseButtonEvent);
         void dispatchMouseScrollEvent(SDL_MouseWheelEvent& mouseWheelEvent);
 
-        IOEventQueue* ptrEventQ;
-        std::unordered_map<IOEventType, bool>& refKeyStates;
-        std::unordered_map<IOEventType, bool>& refMouseButtonStates;
-        Math2D::Point2D<int>& refMousePosition;
-        Math2D::Point2D<int>& refScrollDirection;
+		IOEventQueue* ptrEventQ;                                        /// Pointer to the event queue to dispatch events to.
+		std::unordered_map<IOEventType, bool>& refKeyStates;            /// Reference to the key states map.
+		std::unordered_map<IOEventType, bool>& refMouseButtonStates;	/// Reference to the mouse button states map.
+		Math2D::Point2D<int>& refMousePosition;							/// Reference to the mouse position.
+		Math2D::Point2D<int>& refScrollDirection;						/// Reference to the mouse scroll direction.
 	};
 }
