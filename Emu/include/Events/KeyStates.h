0aaed85eff3818b1e981d144bef3d1224dbda95d
Kael Johnson <kaeljohnson6@gmail.com>
2026-01-04 08:36:45 -0600

    More comments

--- Emu/include/Events/KeyStates.h
+++ Emu/include/Events/KeyStates.h
@@ -7,6 +7,9 @@ namespace Engine
 {
 	using KeyStatesMap = std::unordered_map<IOEventType, bool>;
 
+	/**
+	 * @brief Manages the states of keys (pressed/released) for IO events.
+	 */
 	struct KeyStates
 	{
 		static const KeyStatesMap& GetKeyStates() { return m_keyStates; }
